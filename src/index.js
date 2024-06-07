9c14ad050e5ad118338813fd4790cce457997f5f
Axel <maxelolsson@gmail.com>
2024-06-07 19:54:50 -0400

    Fix weird flowbite issue

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import "./style.css";
 import "flowbite";
 import navigate from "./scripts/navigation";
 
+function reset() {
+    const targets = document.querySelectorAll('[drawer-backdrop=""]');
+    if (targets.length > 1) {
+        targets[0].remove();
+    }
+}
+
+window.reset = reset;
 window.navigate = navigate;
--- src/scripts/portrait.js
+++
@@ -1,8 +0,0 @@
-import portraitUrl from "../photos/portrait.jpeg";
-
-const portrait = () => {
-  const imgTag = document.getElementById("portrait");
-  imgTag.src = portraitUrl;
-};
-
-export default portrait;
